a3e2cddc6839fc83d7f687cc9887886ff8823f50
changyixing <https://gitee.com/changyixing/cyx.git>
2023-02-15 17:12:27 +0800

    9.0

--- jinli-admin/src/main/java/com/xinzhi/admin/JinliAdminAppliocation.java
+++ jinli-admin/src/main/java/com/xinzhi/admin/JinliAdminAppliocation.java
@@ -3,13 +3,20 @@ package com.xinzhi.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @MapperScan("com.xinzhi.admin.dao")
-public class JinliAdminAppliocation {
+public class JinliAdminAppliocation extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(JinliAdminAppliocation.class,args);
 //        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(JinliAdminAppliocation.class);
     }
 }
